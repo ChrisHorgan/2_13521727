9d95772bdba31723cff127e5351f4c51e2849d38
Chris Horgan <cdhorgan@gmail.com>
2018-10-23 22:18:49 +0100

    Initial commit

--- account.rb
+++ account.rb
@@ -1,3 +1,19 @@
 class Account
-  
+  def initialize
+    @id
+    @balance = 0
+  end
+
+  def deposit(amount)
+
+  end
+
+  def withdraw(amount)
+
+  end
+
+  def to_s
+
+  end
+
 end
--- bank.rb
+++ bank.rb
@@ -1,4 +1,18 @@
 class Bank
+  def initialize
 
+  end
+
+  def add_account
+
+  end
+
+  def deposit
+
+  end
+
+  def to_s
+
+  end
 
 end
